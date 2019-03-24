55c1b072f0e561820d4e7d573295cd16db0a6072
Himanshu Kushwah <himanshu.kushwaha@drivezy.com>
2019-03-24 16:11:35 +0530

    restaurant scen cart button added

--- App/Screen/Restaurant-Screen/restaurant.screen.js
+++ App/Screen/Restaurant-Screen/restaurant.screen.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Dimensions, Platform } from 'react-native';
 import { ScaledSheet } from 'react-native-size-matters';
-import { View, ScrollView, TouchableOpacity, Text } from '../../UIComponents';
+import { View, ScrollView, TouchableOpacity, Text, Image } from '../../UIComponents';
 import { FAB, Portal } from 'react-native-paper';
 import MDI from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Colors } from '../../Constants/theme.constants';
 import BottomSheetService from '../../Services/bottomSheet.service';
 import ReactNativeParallaxHeader from 'react-native-parallax-header';
 import { Switch } from 'react-native-paper';
+
 
 var { height, width } = Dimensions.get('window');
 var SCREEN_HEIGHT = height;
@@ -47,40 +48,137 @@ class RestaurantScreen extends Component {
         </View>
     )
 
-    RenderContent = () => {
-        const { isVeg } = this.state;
+    RestaurantHighlights = () => (
+        <View style={styles.upperContentWrapper} >
+            <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} >
+                <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }} >
+                    <MDI name="star" size={25} color={Colors.SecondaryText} />
+                    <Text>4.2</Text>
+                </View>
+                <Text>100+Ratings</Text>
+            </View>
+            <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} >
+                <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }} >
+                    <Text>25 mins</Text>
+                </View>
+                <Text>Delivery time</Text>
+            </View>
+            <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} >
+                <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }} >
+                    <Text>300</Text>
+                </View>
+                <Text>Fot two</Text>
+            </View>
+        </View>
+    )
 
-        return (
+    VegOnly = () => (
+        <View style={{ padding: 20, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }} >
+            <Text>VEG ONLY</Text>
+            <Switch
+                value={this.state.isVeg}
+                color={Colors.Success}
+                onValueChange={() => { this.setState({ isVeg: !this.state.isVeg }); }}
+            />
+        </View>
+    )
+
+    FoodCard = () => (
+        <View style={{ flex: 1, alignItems: 'center', height: 200 }} >
+            <Image resizeMode="cover" style={{ width: 180, height: 140 }} source={{ uri: 'https://drop.ndtv.com/albums/COOKS/chinese-vegetarian/rollsspring_640x480.jpg?output-format=webp' }} />
             <View>
-                <View style={styles.upperContentWrapper} >
-                    <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} >
-                        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }} >
-                            <MDI name="star" size={25} color={Colors.SecondaryText} />
-                            <Text>4.2</Text>
-                        </View>
-                        <Text>100+Ratings</Text>
-                    </View>
-                    <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} >
-                        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }} >
-                            <Text>25 mins</Text>
-                        </View>
-                        <Text>Delivery time</Text>
-                    </View>
-                    <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} >
-                        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }} >
-                            <Text>300</Text>
-                        </View>
-                        <Text>Fot two</Text>
-                    </View>
+                <Text style={{ fontSize: 10, textAlign: 'left' }} >Starter</Text>
+                <Text style={{ fontSize: 16, color: Colors.PrimaryText }} >Nachos with cheese</Text>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }} >
+                    <Text style={{ fontSize: 16 }} >170</Text>
+
+                    <TouchableOpacity
+                        onPress={() => { }}
+                        style={{ width: 70, height: 30, alignItems: 'center', justifyContent: 'center', borderWidth: 1, borderColor: Colors.Success }}
+                    >
+                        <Text style={{ fontSize: 14, color: Colors.Success }} >Add</Text>
+                    </TouchableOpacity>
                 </View>
-                <View style={{ padding: 20, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }} >
-                    <Text>VEG ONLY</Text>
-                    <Switch
-                        value={isVeg}
-                        color={Colors.Success}
-                        onValueChange={() => { this.setState({ isVeg: !isVeg }); }}
-                    />
-                </View>
+            </View>
+        </View>
+    )
+
+    FoodItem = () => (
+        <View style={{ flex: 1, alignItems: 'space-between', flexDirection: 'row', height: 50 }} >
+            <View style={{ flex: 1, alignItems: 'flex-start', justifyContent: 'center' }} >
+                <Text style={{ fontSize: 16, color: Colors.PrimaryText }} >Nachos with cheese</Text>
+                <Text style={{ fontSize: 10, textAlign: 'left' }} >Starter</Text>
+            </View>
+            <View style={{ flex: 0.5, justifyContent: 'center', alignItems: 'center' }} >
+                <Text style={{ fontSize: 16 }} >170</Text>
+            </View>
+            <View style={{ flex: 0.5, justifyContent: 'center', alignItems: 'center' }} >
+                <TouchableOpacity
+                    onPress={() => { }}
+                    style={{ width: 70, height: 30, alignItems: 'center', justifyContent: 'center', borderWidth: 1, borderColor: Colors.Success }}
+                >
+                    <Text style={{ fontSize: 14, color: Colors.Success }} >Add</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+
+    RecommendedSection = () => (
+        <View style={{ flexDirection: 'column' }} >
+            <View style={{ flexDirection: 'row', marginBottom: 20 }} >
+                <this.FoodCard />
+                <this.FoodCard />
+            </View>
+            <View style={{ flexDirection: 'row', marginBottom: 20 }} >
+                <this.FoodCard />
+                <this.FoodCard />
+            </View>
+            <View style={{ flexDirection: 'row', marginBottom: 20 }} >
+                <this.FoodCard />
+                <this.FoodCard />
+            </View>
+            <View style={{ flexDirection: 'row', marginBottom: 20 }} >
+                <this.FoodCard />
+                <this.FoodCard />
+            </View>
+
+            <View style={{ flexDirection: 'row', marginBottom: 20, paddingLeft: 10, paddingRight: 10 }} >
+                <this.FoodItem />
+            </View>
+
+            <View style={{ flexDirection: 'row', marginBottom: 20, paddingLeft: 10, paddingRight: 10 }} >
+                <this.FoodItem />
+            </View>
+
+            <View style={{ flexDirection: 'row', marginBottom: 20, paddingLeft: 10, paddingRight: 10 }} >
+                <this.FoodItem />
+            </View>
+
+            <View style={{ flexDirection: 'row', marginBottom: 20, paddingLeft: 10, paddingRight: 10 }} >
+                <this.FoodItem />
+            </View>
+
+            <View style={{ flexDirection: 'row', marginBottom: 20, paddingLeft: 10, paddingRight: 10 }} >
+                <this.FoodItem />
+            </View>
+
+            <View style={{ flexDirection: 'row', marginBottom: 20, paddingLeft: 10, paddingRight: 10 }} >
+                <this.FoodItem />
+            </View>
+
+            <View style={{ flexDirection: 'row', marginBottom: 20, paddingLeft: 10, paddingRight: 10 }} >
+                <this.FoodItem />
+            </View>
+
+        </View>
+    )
+
+    RenderContent = () => {
+        return (
+            <View >
+                <this.RestaurantHighlights />
+                <this.VegOnly />
+                <this.RecommendedSection />
             </View>
         )
     }
@@ -122,6 +220,19 @@ class RestaurantScreen extends Component {
                     )}
                     onPress={() => BottomSheetService.open('MENU')}
                 />
+
+                <View style={{ height: 50, alignItems: 'center', justifyContent: 'space-between', flexDirection: 'row', padding: 10, position: 'absolute', bottom: 0, left: 0, right: 0, backgroundColor: Colors.Success }} >
+                    <Text style={{ color: Colors.Surface }} >1 Item  |  270</Text>
+
+                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }} >
+                        <MDI
+                            name={'cart'}
+                            size={22}
+                            color={Colors.Surface}
+                        />
+                        <Text style={{ fontSize: 16, color: Colors.Surface }} >VIEW CART</Text>
+                    </View>
+                </View>
             </View>
         )
     }
@@ -138,7 +249,7 @@ const styles = ScaledSheet.create({
         position: 'absolute',
         margin: 16,
         right: 0,
-        bottom: 0,
+        bottom: 50,
         backgroundColor: Colors.Info
     },
     navContainer: {
